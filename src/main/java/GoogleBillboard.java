e2152194dcaa453860cef76228eb5e7d667b8b05
auhsu10 <143027511+auhsu10@users.noreply.github.com>
2024-01-19 12:33:09 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -1,18 +1,15 @@
 public class GoogleBillboard {
   public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
   public double digits=0;
-  public void setup(){            
+  public static void main(){         
     //your code here 
     for(int i=1;i<e.length()-10;i++){
       digits=Double.parseDouble(e.substring(i,i+10));
       if(isPrime(digits)==true){
-        System.out.println(digits+"");
+        System.out.println(digits);
         break;
       }
     }
-  }
-  public void draw(){
-    
   }
   public static boolean isPrime(double dNum){   
     //your code here
